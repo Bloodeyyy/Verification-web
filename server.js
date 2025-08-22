7da17dcabe7d4226be7b114a61889875dd8018cb
Bloodeyyy <bloodeystill90@gmail.com>
2025-08-22 19:44:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import fetch from "node-fetch";
+import axios from "axios";
 import User from "./models/User.js";
 
 dotenv.config();
@@ -35,26 +35,27 @@ app.get("/callback", async (req, res) => {
 
   try {
     // Exchange code for access token
-    const tokenResponse = await fetch("https://discord.com/api/oauth2/token", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams({
+    const tokenResponse = await axios.post(
+      "https://discord.com/api/oauth2/token",
+      new URLSearchParams({
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
         grant_type: "authorization_code",
         code,
         redirect_uri: process.env.REDIRECT_URI,
       }),
-    });
+      { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+    );
 
-    const tokenData = await tokenResponse.json();
+    const tokenData = tokenResponse.data;
     if (!tokenData.access_token) return res.send("Error getting access token!");
 
     // Get user info
-    const userResponse = await fetch("https://discord.com/api/users/@me", {
+    const userResponse = await axios.get("https://discord.com/api/users/@me", {
       headers: { Authorization: `Bearer ${tokenData.access_token}` },
     });
-    const userData = await userResponse.json();
+
+    const userData = userResponse.data;
 
     // Save user in DB
     await User.findOneAndUpdate(

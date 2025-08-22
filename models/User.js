e7ad79eb3d6e6dcd17a545ebe8bd23415c89bbc7
Bloodeyyy <bloodeystill90@gmail.com>
2025-08-22 23:17:21 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,8 +2,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  discordId: { type: String, required: true },
+  discordId: { type: String, required: true, unique: true },
   username: { type: String, required: true },
+  verified: { type: Boolean, default: false }, // ✅ easy filter
+  accessToken: { type: String }, // ✅ guild join ke liye
   verifiedAt: { type: Date, default: Date.now }
 });
 

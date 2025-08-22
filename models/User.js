7e16c08bf10a767249c50f17ddb6029df63b4fcc
Bloodeyyy <bloodeystill90@gmail.com>
2025-08-22 17:53:33 +0530

    Create models/User.js

---
+++ models/User.js
@@ -0,0 +1,10 @@
+// models/User.js
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  discordId: { type: String, required: true },
+  username: { type: String, required: true },
+  verifiedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", userSchema);

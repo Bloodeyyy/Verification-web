4cb79a5c06fa590ffdc127c5c73ba0794f741b26
Bloodeyyy <bloodeystill90@gmail.com>
2025-08-22 17:22:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-.
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Home page
+app.get("/", (req, res) => {
+  res.send("✅ Verification Website is Running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
